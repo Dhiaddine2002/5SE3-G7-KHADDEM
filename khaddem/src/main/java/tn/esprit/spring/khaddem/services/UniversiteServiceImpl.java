96210d1bd777e7b2c4f6518d1b5943e531464222
dhieff73 <dhieff.14@gmail.com>
2023-11-14 02:11:41 +0100

    bugs fix

--- khaddem/src/main/java/tn/esprit/spring/khaddem/services/UniversiteServiceImpl.java
+++ khaddem/src/main/java/tn/esprit/spring/khaddem/services/UniversiteServiceImpl.java
@@ -3,12 +3,14 @@ package tn.esprit.spring.khaddem.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
 import tn.esprit.spring.khaddem.entities.Departement;
 import tn.esprit.spring.khaddem.entities.Universite;
 import tn.esprit.spring.khaddem.repositories.DepartementRepository;
 import tn.esprit.spring.khaddem.repositories.UniversiteRepository;
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -37,8 +39,16 @@ public class UniversiteServiceImpl implements  IUniversiteService{
 
     @Override
     public Universite retrieveUniversite(Integer idUniversite) {
-        return universiteRepository.findById(idUniversite).get();
+        Optional<Universite> optionalUniversite = universiteRepository.findById(idUniversite);
+
+        if (optionalUniversite.isPresent()) {
+            return optionalUniversite.get();
+        } else {
+
+            throw new NotFoundException("Universite not found for ID: " + idUniversite);
+        }
     }
+
 
     @Transactional
     public void assignUniversiteToDepartement(Integer universiteId, Integer departementId) {
